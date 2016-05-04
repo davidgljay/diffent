8835a1ee3ebb00d7d80a536aa2652126a7c40fbc
David Jay <davidgljay@gmail.com>
2016-05-03 23:48:45 -0400

    Adding sctructs to DocSet.

--- DocSet.go
+++ DocSet.go
@@ -1,5 +1,27 @@
 package entropy
 
+type DocSet struct {
+	documents []string
+	words []word
+}
+
+
+type word struct {
+	freq uint32
+	prob_array []probability
+	mentions []mention
+}
+
+type mention struct {
+	ts uint64
+	entropy float32
+	top []probability
+}
+
+type probability struct {
+	word string
+	prob float32
+}
 //TODO: Create struct
 
 //TODO: Iterate through docs for TFIF
